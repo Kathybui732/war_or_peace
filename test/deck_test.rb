24482b753dba9fae55173fe4534764bb2484ce67
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-04 19:46:55 -0600

    Add add a card test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -92,16 +92,18 @@ class DeckTest < Minitest::Test
     assert_equal 50.0, deck.percent_high_ranking
   end
 
-  # def test_can_it_add_card
-  #   card1 = Card.new(:diamond, "Queen", 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, "Ace", 14)
-  #
-  #   cards = [card1, card2, card3]
-  #
-  #   deck = Deck.new(cards)
-  #
-  #   card4 = Card.new(:club, "5", 5)
-  #
-  #   deck.add_card(card4)
+  def test_can_it_add_card
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+
+    card4 = Card.new(:club, "5", 5)
+
+    assert_equal [card2, card3, card4], deck.add_card(card4)
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 33.33, deck.percent_high_ranking
+  end
 end
