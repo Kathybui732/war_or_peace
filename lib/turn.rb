86e738f1eb0a8829de78633005861f3269c65bc8
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-07 09:42:33 -0600

    Create initial method for winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,8 @@
 class Turn
-  attr_reader :player1, :player2, :spoils_of_war
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war,
+              :type
 
   def initialize(player1, player2)
     @player1 = player1
@@ -7,4 +10,32 @@ class Turn
     @spoils_of_war = []
     @type = :basic
   end
+
+  def winner
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      @type = :basic
+      player1
+    elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+      @type = :basic
+      player2
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      @type = :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+        player2
+      elsif player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+        @type = :mutually_assured_destruction
+        "No winner"
+      end
+    end
+  end
+
+  # def pile_cards
+  #   Cards played from player1 and player2 are stacked into spoils of war
+  # end
+  #
+  # def award_spoils(winner)
+  #   winner.deck << @spoils_of_war; cards from spoils of war added to winner dieck
+  # end
 end
