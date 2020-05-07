2969e09460eab1d6afc475b181ff190136055330
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-07 11:14:46 -0600

    Create workable basic turn pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,10 +31,22 @@ class Turn
     end
   end
 
-  # def pile_cards
-  #   Cards played from player1 and player2 are stacked into spoils of war
-  # end
-  #
+  def pile_cards
+    # Cards played from player1 and player2 are stacked into spoils of war
+    if @type == :basic
+      @spoils_of_war << player1.deck.cards[0]
+      @spoils_of_war << player2.deck.cards[0]
+    elsif @type == :war
+      @spoils_of_war << player1.deck.cards[0..2]
+      @spoils_of_war << player2.deck.cards[0..2]
+    elsif @type == :mutually_assured_destruction
+      3.times do
+        player1.remove_card
+        player2.remove_card
+      end
+    end
+  end
+
   # def award_spoils(winner)
   #   winner.deck << @spoils_of_war; cards from spoils of war added to winner dieck
   # end
