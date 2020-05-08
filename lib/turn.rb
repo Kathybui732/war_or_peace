83c984ea6a10b833c6456d6aa9fce3b846222197
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-07 20:50:50 -0600

    Modify pile cards adn award spoils methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,53 +1,61 @@
 class Turn
   attr_reader :player1,
               :player2,
-              :spoils_of_war,
-              :type
+              :spoils_of_war
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @type = :basic
+  end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    else
+      :basic
+    end
   end
 
   def winner
-    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-      @type = :basic
-      player1
-    elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
-      @type = :basic
-      player2
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      @type = :war
-      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-        player1
-      elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
-        player2
-      elsif player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-        @type = :mutually_assured_destruction
-        "No winner"
+    if type == :mutually_assured_destruction
+      "No Winner"
+    elsif type == :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        @player1
+      elsif @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+        @player2
+      end
+    elsif type == :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        @player1
+      elsif @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+        @player2
       end
     end
   end
 
   def pile_cards
     # Cards played from player1 and player2 are stacked into spoils of war
-    if @type == :basic
-      @spoils_of_war << player1.deck.cards[0]
-      @spoils_of_war << player2.deck.cards[0]
-    elsif @type == :war
-      @spoils_of_war << player1.deck.cards[0..2]
-      @spoils_of_war << player2.deck.cards[0..2]
-    elsif @type == :mutually_assured_destruction
+    if type == :basic
+      @spoils_of_war.push(player1.deck.remove_card, player2.deck.remove_card)
+    elsif type == :war
+      @spoils_of_war.push(player1.deck.remove_card, player1.deck.remove_card, player1.deck.remove_card, player2.deck.remove_card, player2.deck.remove_card, player2.deck.remove_card)
+    elsif type == :mutually_assured_destruction
       3.times do
-        player1.remove_card
-        player2.remove_card
+        player1.deck.remove_card
+        player2.deck.remove_card
       end
     end
   end
 
-  # def award_spoils(winner)
-  #   winner.deck << @spoils_of_war; cards from spoils of war added to winner dieck
-  # end
+  def award_spoils(turn_winner)
+    if type == :mutually_assured_destruction
+      @spoils_of_war = []
+    elsif type == :war || :basic
+      turn_winner.deck.add_card(@spoils_of_war).flatten!
+    end
+  end
 end
