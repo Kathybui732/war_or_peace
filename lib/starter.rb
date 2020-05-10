a106e1202440330bd25d062febb21a90cabee006
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-10 17:59:42 -0600

    Finalize starter class

--- lib/starter.rb
+++ lib/starter.rb
@@ -8,15 +8,15 @@ class Starter
   def start
     until turn.game_over? do
       turn.next_turn
-      puts "Turn #{turn.turn_number}: #{turn.game_messages}"
+      print "Turn #{turn.turn_number}: #{turn.game_messages}"
       turn.pile_cards
       turn.award_spoils(turn.winner)
       turn.empty_spoils
       if turn.game_over?
         if turn.turn_number == 1000000 || turn.winner == "No Winner"
-          puts "---- DRAW ----"
+          print "---- DRAW ----"
         else
-          puts "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
+          print "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
         end
       end
     end
