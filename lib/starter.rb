d29f9d07e233ee491ebf80bd202d7d95f65ea89f
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-10 22:21:19 -0600

    Refactor card test

--- lib/starter.rb
+++ lib/starter.rb
@@ -6,14 +6,14 @@ class Starter
   end
 
   def start
-    until turn.game_over? do
-      turn.next_turn
-      print "Turn #{turn.turn_number}: #{turn.game_messages}"
-      turn.pile_cards
-      turn.award_spoils(turn.winner)
-      turn.empty_spoils
-      if turn.game_over?
-        if turn.turn_number == 1000000 || turn.winner == "No Winner"
+    until @turn.game_over? do
+      @turn.next_turn
+      print "Turn #{@turn.turn_number}: #{@turn.game_messages}"
+      @turn.pile_cards
+      @turn.award_spoils(turn.winner)
+      @turn.empty_spoils
+      if @turn.game_over?
+        if @turn.turn_number == 1000000 || @turn.winner == "No Winner"
           print "---- DRAW ----"
         else
           print "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
