7596f3fbed6d56ee26ea136697d5e47c17d54fd4
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-14 12:14:36 -0600

    Create new methods to refactor code

--- lib/player.rb
+++ lib/player.rb
@@ -9,4 +9,16 @@ class Player
   def has_lost?
     @deck.cards == []
   end
+
+  def third_card
+    @deck.rank_of_card_at(2)
+  end
+
+  def first_card
+    @deck.rank_of_card_at(0)
+  end
+
+  def lose_card
+    @deck.remove_card
+  end
 end
