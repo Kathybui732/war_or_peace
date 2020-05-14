682aebd58ec6aee8936e49296eca934854c53bd0
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-14 12:15:17 -0600

    Update starter code

--- lib/starter.rb
+++ lib/starter.rb
@@ -10,13 +10,13 @@ class Starter
       @turn.next_turn
       print "Turn #{@turn.turn_number}: #{@turn.game_messages}"
       @turn.pile_cards
-      @turn.award_spoils(turn.winner)
+      @turn.award_spoils(@turn.winner)
       @turn.empty_spoils
       if @turn.game_over?
         if @turn.turn_number == 1000000 || @turn.winner == "No Winner"
           print "---- DRAW ----"
         else
-          print "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
+          print "*~*~*~* #{@turn.winner.name} has won the game! *~*~*~*"
         end
       end
     end
